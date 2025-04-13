c1172a4938801caf0b9fa3368e50c02d6449d2cf
Edward Mahama <mahamaek@gmail.com>
2025-04-13 18:37:26 -0400

    conditionals

---
+++ Revision/Module-2/weather.py
@@ -0,0 +1,7 @@
+temp = int(input("Enter temperature: "))
+if temp > 80:
+    print("It is too hot. \nStay inside")
+elif temp < 80 and temp > 55:
+    print("Enjoy outdoors!")
+else:
+    print("The weather is pretty cold!. YOu better keep warm.")
