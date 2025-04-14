af8be5c198b8297531e135977222c798b4f7ccb7
Edward Mahama <mahamaek@gmail.com>
2025-04-13 21:30:39 -0400

    sum example

---
+++ Revision/Module-3/expenses.py
@@ -0,0 +1,8 @@
+expenses = [10.50, 8, 5, 15, 20, 5, 3]
+total = sum(expenses)
+print(f"The total is : ${total}")
+
+total_exp = 0
+for exp in expenses:
+    total_exp += exp
+print(f"The total is : ${total_exp}")
